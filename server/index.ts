f6d0bed8cd12486bfa5ff9d9058f6db15611e08c
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-10-25 00:01:15 +0530

    Setting up basic websocket on the bun server

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,19 @@
+import { createBunWebSocket } from "hono/bun";
 import app from "./app.ts";
 
-Bun.serve({ fetch: app.fetch });
+Bun.serve({
+  fetch: app.fetch,
+  websocket: {
+    open(ws) {
+      console.log("Client Connected", ws);
+    },
+    message(ws, message) {
+      console.log("Received Message is:", message);
+    },
+    close(ws, code, message) {
+      console.log("Connection Closed", code, message);
+    },
+  },
+});
 
 console.log("Server Started");
--- server/routes/room.ts
+++ server/routes/room.ts
@@ -1,12 +1,55 @@
+// import { Hono } from "hono";
+// import { zValidator } from "@hono/zod-validator";
+// import { createRoomSchema } from "../db/sharedTypes";
+// import { createRoom } from "../controllers/room";
+// import { createBunWebSocket } from "hono/bun";
+
+// const { upgradeWebSocket } = createBunWebSocket();
+
+// const roomRoutes = new Hono()
+//   .post("/", zValidator("json", createRoomSchema), createRoom)
+//   .get(
+//     "/test",
+//     upgradeWebSocket((c) => {
+//       return {
+//         onOpen(event, ws) {
+//           console.log("client connected", event, ws);
+//           ws.send("Hello from server");
+//         },
+//       };
+//     }),
+//   );
+
+// export default roomRoutes;
+// export type RoomTypes = typeof roomRoutes;
+
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { createRoomSchema } from "../db/sharedTypes";
 import { createRoom } from "../controllers/room";
+import { createBunWebSocket } from "hono/bun";
 
-const roomRoutes = new Hono().post(
-  "/",
-  zValidator("json", createRoomSchema),
-  createRoom
-);
+const { upgradeWebSocket } = createBunWebSocket();
+
+const roomRoutes = new Hono()
+  .post("/", zValidator("json", createRoomSchema), createRoom)
+  .get(
+    "/ws",
+    upgradeWebSocket((c) => {
+      return {
+        onOpen(event, ws) {
+          console.log("Client Connected"), ws.send("Hello from the server");
+        },
+        onMessage(event, ws) {
+          console.log("Received Message", event.data);
+          console.log(`Server received: ${event.data}`);
+        },
+        onClose(event, ws) {
+          console.log("Client disconnected");
+        },
+      };
+    }),
+  );
 
 export default roomRoutes;
+export type RoomTypes = typeof roomRoutes;
